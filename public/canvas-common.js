33c5350c435eb17f5c9f76b85578d3733d7adc07
Allen Chum <chumwai@gmail.com>
2018-02-14 17:25:30 +0800

    add node

---
+++ public/canvas-common.js
@@ -0,0 +1,81 @@
+let canvasDraft = document.getElementById('canvasDraft');
+let contextDraft = canvasDraft.getContext('2d');
+let canvasReal = document.getElementById('canvasReal');
+let contextReal = canvasReal.getContext('2d');
+let currentFunction;
+let dragging = false;
+
+
+
+//ParentFunction 
+class ParentFunction{
+    constructor(){}
+    onMouseDown(){}
+    onDragging(){}
+    onMouseMove(){}
+    onMouseUp(){}
+    onMouseLeave(){}
+    onMouseEnter(){}
+};
+
+currentFunction = new ParentFunction();
+
+
+//call function to 'currentFunction' when user click the button  
+
+$('#add-node').click(function(){
+    currentFunction = new AddNode(contextReal, contextDraft);
+})
+
+
+//Applu currentFunction to Canvas
+
+$('#canvasDraft').mousedown(function(e){
+    contextDraft.clearRect(0, 0, contextDraft.canvas.width, contextDraft.canvas.height);
+    let mouse = {
+        x: e.pageX - this.offsetLeft,      //Allen: object "mouse" store x-coordinate & y-coordinate of mouse event
+        y: e.pageY - this.offsetTop        //instead of [mouseX,mouseY]
+    };                                     //I try keep using 'object' to store data, make it more consist.
+    currentFunction.onMouseDown(mouse,e);
+    dragging = true;
+});
+$('#canvasDraft').mousemove(function(e){
+    let mouse = {
+        x: e.pageX - this.offsetLeft,
+        y: e.pageY - this.offsetTop
+    };  
+    if (dragging){
+     currentFunction.onDragging(mouse,e);
+    }else{currentFunction.onMouseMove(mouse,e);}
+});
+
+$('#canvasDraft').mouseup(function(e){
+    dragging = false;
+    let mouse = {
+        x: e.pageX - this.offsetLeft,
+        y: e.pageY - this.offsetTop
+    };
+    currentFunction.onMouseUp(mouse,e);
+    contextReal.drawImage(canvasDraft,0,0);
+    contextDraft.clearRect(0, 0, contextDraft.canvas.width, contextDraft.canvas.height);
+});
+
+$('#canvasDraft').mouseleave(function(e){
+    dragging = false;
+    let mouse =  {
+        x: e.pageX - this.offsetLeft,
+        y: e.pageY - this.offsetTop
+    }; 
+    currentFunction.onMouseLeave(mouse,e);
+    contextReal.drawImage(canvasDraft,0,0);
+    contextDraft.clearRect(0, 0, contextDraft.canvas.width, contextDraft.canvas.height);
+});
+$('#canvasDraft').mouseenter(function(e){
+    let mouse = {
+        x: e.pageX - this.offsetLeft,
+        y: e.pageY - this.offsetTop
+    }; 
+    currentFunction.onMouseEnter(mouse,e);
+});
+
+
