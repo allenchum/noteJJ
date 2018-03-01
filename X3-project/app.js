6fb4b65bc5c9848dd92bbfffc9f29ec902721a7c
gordancso <gordan.chungsaion@gmail.com>
2018-03-01 08:56:54 +0800

    3/1

--- X3-project/app.js
+++ X3-project/app.js
@@ -95,7 +95,7 @@ app.get('/auth/facebook/callback',
             facebookID: req.user.profile.id,
             created_at: knex.fn.now()
           }).into('users');
-          console.log("user inserted to table.");
+          console.log("user inserted to table.", userNotes);
         }
       })
       .then(() => {
@@ -127,94 +127,115 @@ app.get('/', isLoggedIn, (req, res) => {
   res.render(__dirname + '/views/partial/create-form', {
     username: req.user.profile.displayName,
     profileLink: req.user.profile.photos[0].value,
-    firstName: req.user.profile.name.givenName
+    firstName: req.user.profile.name.givenName,
   });
 });
 
-// post request for creating new note
-app.post('/create-new', isLoggedIn, (req, res) => {
-  upload(req, res, (err) => {
-    console.log(req.file)
-    if (err) {
-      console.log(err)
-    } else {
-      return knex.insert({
-          title: req.body.title,
-          imagePath: req.file.path,
-          image_x1: 100,
-          image_y1: 100,
-          created_at: knex.fn.now(),
-          user_id: 4
-        }).into('notes')
-        .then(() => {
-          res.render(__dirname + '/views/partial/edit-page', {
-            title: req.body.title,
-            picPath: req.file.path,
-            canvas_width: req.body.width,
-            canvas_height: req.body.height
-          });
-          console.log("notes inserted")
-        })
-    }
-  })
-});
-
-// set storage engine
-const storage = multer.diskStorage({
-  destination: __dirname + '/views/uploads',
-  filename: function(req, file, cb) {
-    cb(null, file.fieldname + '-' + Date.now() +
-      path.extname(file.originalname))
-  }
-})
-
-const upload = multer({
-  storage: storage,
-  limits: {
-    fileSize: 1000000
-  },
-  fileFilter: function(req, file, cb) {
-    checkFileType(file, cb);
-  }
-}).single('imageUpload')
-
-function checkFileType(file, cb) {
-  const filetypes = /jpeg|jpg|png|gif/;
-  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
-  const mimetype = filetypes.test(file.mimetype);
-  if (mimetype && extname) {
-    return cb(null, true);
-  } else {
-    cb('Error: Images only')
-  }
+function getUserNotes(userID) {
+  let query = knex.select("title", "imagePath").from("notes")
+    .innerJoin("users", "user_id", "users.id")
+    .where("facebookID", userID);
+  query.then((rows) => {
+    return rows;
+  });
 }
-
-// re-direct user to create new form when they get into "create-new" route
-// need to review the routing of this re-direct
-app.get('/create-new', isLoggedIn, (req, res) => {
-  res.render(__dirname + '/views/partial/create-form');
-});
-
-// serving static files in folder "views"
-app.use(express.static(__dirname + "/views"));
-
-
-// get input from drawing insert as a form of object
-app.post('/create-new/new-node', (req, res) => {
-  // each time to insert node to nodes table
-  return knex.insert({
-    node_x1: req.body.x1,
-    node_y1: req.body.y1,
-    node_x2: req.body.x2,
-    node_y2: req.body.y2,
-    node_title: req.body.title,
-    node_content: req.body.content,
-    note_id: 1,
-  }).into('nodes');
+app.get('/displayNotes', isLoggedIn, (req, res) => {
+      let query = knex.select("title", "imagePath").from("notes")
+        .innerJoin("users", "user_id", "users.id")
+        .where("facebookID", req.user.profile.id);
+      query.then((rows) => {
+        res.render(__dirname + '/views/partial/display-notes', {
+          notesDisplay: rows,
+        });
+        console.log(rows)
+      });
 })
 
 
-//express port 8000
-app.listen(port, () => {
-  console.log("listening to port", port);
-})
+      // post request for creating new note
+      app.post('/create-new', isLoggedIn, (req, res) => {
+        upload(req, res, (err) => {
+          console.log(req.file)
+          if (err) {
+            console.log(err)
+          } else {
+            return knex.insert({
+                title: req.body.title,
+                imagePath: req.file.filename,
+                image_x1: 100,
+                image_y1: 100,
+                created_at: knex.fn.now(),
+                user_id: 4
+              }).into('notes')
+              .then(() => {
+                res.render(__dirname + '/views/partial/edit-page', {
+                  title: req.body.title,
+                  picPath: req.file.path,
+                  canvas_width: req.body.width,
+                  canvas_height: req.body.height
+                });
+                console.log("notes inserted")
+              })
+          }
+        })
+      });
+
+      // set storage engine
+      const storage = multer.diskStorage({
+        destination: __dirname + '/views/uploads',
+        filename: function(req, file, cb) {
+          cb(null, file.fieldname + '-' + Date.now() +
+            path.extname(file.originalname))
+        }
+      })
+
+      const upload = multer({
+        storage: storage,
+        limits: {
+          fileSize: 1000000
+        },
+        fileFilter: function(req, file, cb) {
+          checkFileType(file, cb);
+        }
+      }).single('imageUpload')
+
+      function checkFileType(file, cb) {
+        const filetypes = /jpeg|jpg|png|gif/;
+        const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+        const mimetype = filetypes.test(file.mimetype);
+        if (mimetype && extname) {
+          return cb(null, true);
+        } else {
+          cb('Error: Images only')
+        }
+      }
+
+      // re-direct user to create new form when they get into "create-new" route
+      // need to review the routing of this re-direct
+      app.get('/create-new', isLoggedIn, (req, res) => {
+        res.render(__dirname + '/views/partial/create-form');
+      });
+
+      // serving static files in folder "views"
+      app.use(express.static(__dirname + "/views"));
+
+
+      // get input from drawing insert as a form of object
+      app.post('/create-new/new-node', (req, res) => {
+        // each time to insert node to nodes table
+        return knex.insert({
+          node_x1: req.body.x1,
+          node_y1: req.body.y1,
+          node_x2: req.body.x2,
+          node_y2: req.body.y2,
+          node_title: req.body.title,
+          node_content: req.body.content,
+          note_id: 1,
+        }).into('nodes');
+      })
+
+
+      //express port 8000
+      app.listen(port, () => {
+        console.log("listening to port", port);
+      })
